bc260ce4957a4058f202c696bd3edf9f99ce3a43
SDQ-BQA <sdmbqa420@gmail.com>
2024-07-17 00:37:13 +0530

    color picker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,6 +1,17 @@
+import { SketchPicker } from 'react-color'
+import { useSnapshot } from 'valtio'
+import state from '../store/Index';
+
 function ColorPicker() {
+  const snap = useSnapshot(state)
   return (
-    <div>ColorPicker</div>
+    <div className="absolute left-full ml-3">
+      <SketchPicker 
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => state.color = color.hex}
+      />
+    </div>
   )
 }
 
