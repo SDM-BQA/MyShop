2714af942bfc78011bed75080951accd9d833215
SDQ-BQA <sdmbqa420@gmail.com>
2024-07-16 13:58:01 +0530

    adding the valtio

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,17 @@
+import {motion, AnimatePresence} from 'framer-motion'
+import { useSnapShot } from 'valtio'
+
+import state from '../store/Index'
+
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion'
+
 function Home() {
+    const snap = useSnapShot(state)
   return (
     <div>Home</div>
   )
