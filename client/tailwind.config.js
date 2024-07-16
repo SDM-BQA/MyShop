235e91a2d995e2a45537320aa1805f3495870e8f
SDQ-BQA <sdmbqa420@gmail.com>
2024-07-16 14:39:08 +0530

    home page and tailwind bug fix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
