438b7195c2576c140d038defc2c605042dd3a4fc
SDQ-BQA <sdmbqa420@gmail.com>
2024-07-16 16:42:48 +0530

    chaging values

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import React, { useRef } from 'react'
 import { easing } from 'maath'
 import { useFrame } from '@react-three/fiber'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
