a93358d5f56aac5b963b154575c31947dbafbfbf
SDQ-BQA <sdmbqa420@gmail.com>
2024-07-16 16:12:21 +0530

    shirt init

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -11,8 +11,13 @@ function Shirt() {
   const logoTexture = useTexture(snap.logoDecal)
   const fullTexture = useTexture(snap.fullDecal)
 
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+
+  const stateString = JSON.stringify(snap);
+
   return (
-    <group>
+    <group
+    key={stateString}>
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
@@ -20,7 +25,26 @@ function Shirt() {
         material-roughness={1}
         dispose={null}
       >
-
+        {/* texture */}
+        {snap.isFullTexture && (
+          <Decal 
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+        {/* logo */}
+         {snap.isLogoTexture && (
+          <Decal 
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
       </mesh>
     </group>
   )
